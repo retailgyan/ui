2d053806d25f46575e98e4d6adf89d9002651a32
Chetana Amancharla <Chetana_Shanbhag@ad.infosys.com>
2019-06-10 16:06:14 +0530

    path route

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
 var app = express();
-app.use(express.static("dist")); 
+app.use(express.static("dist/retailgyan")); 
 app.listen(8080, '127.0.0.1');
 console.log("Listening on port 8080");

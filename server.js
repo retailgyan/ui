03bef56f20a8c2482f7911d20edfc0c0f9082825
Chetana Amancharla <Chetana_Shanbhag@ad.infosys.com>
2019-06-10 16:08:14 +0530

    enable all

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
 var app = express();
 app.use(express.static("dist/retailgyan")); 
-app.listen(8080, '127.0.0.1');
+app.listen(8080, '0.0.0.0');
 console.log("Listening on port 8080");
